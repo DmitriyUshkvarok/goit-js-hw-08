cad88d82f91fef46612d9260eaa61596627d13f9
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2022-11-22 01:06:48 +0200

    ...

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,43 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+// player
+//   .setCurrentTime(JSON.parse(localStorage.getItem('LOCALSTORAGE_KEY')))
+//   .catch(function (error) {
+//     console.error(error);
+//   });
+
+// player.on(
+//   'timeupdate',
+//   throttle(event => {
+//     localStorage.setItem('LOCALSTORAGE_KEY', JSON.stringify(event.seconds));
+//   }, 1000)
+// );
+const player = new Player(document.querySelector('#vimeo-player'));
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-player.setCurrentTime(localStorage.getItem(LOCALSTORAGE_KEY));
+// const stopwatchSecond = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
-player.on('timeupdate', throttle(onPlay, 1000));
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
 
-function onPlay({ seconds }) {
-  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
-}
+      default:
+        break;
+    }
+  });
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(seconds));
+  }, 1000)
+);
